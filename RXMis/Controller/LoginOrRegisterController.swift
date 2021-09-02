14d1a51fe4365152510f1a1ebc755d767cb87d83
yy1234 <yy123456>
2021-09-02 20:00:22 +0800

    登录注册按钮事件处理

--- RXMis/Controller/LoginOrRegisterController.swift
+++ RXMis/Controller/LoginOrRegisterController.swift
@@ -8,17 +8,11 @@
 import UIKit
 
 class LoginOrRegisterController: UIViewController {
+    //MARK: - 声明
     @IBOutlet weak var btLogin: UIButton!
     @IBOutlet weak var btRegister: UIButton!
     
-    //按下
-    @IBAction func touchDown(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(hex: COLOR_PRIMARY)
-    }
-    //抬起
-    @IBAction func touchUp(_ sender: UIButton) {
-        sender.backgroundColor = UIColor.white
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //圆角
@@ -30,8 +24,32 @@ class LoginOrRegisterController: UIViewController {
         btRegister.showColorPrimaryBorder()
         // Do any additional setup after loading the view.
     }
+    //MARK: - 事件
+    //按下
+    @IBAction func touchDown(_ sender: UIButton) {
+        sender.backgroundColor = UIColor(hex: COLOR_PRIMARY)
+    }
+    //抬起
+    @IBAction func touchUp(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.white
+        if sender == btLogin {
+            toLogin()
+        }else{
+            toRegister()
+        }
+    }
+    //按钮以外抬起
+    @IBAction func tochUpOutSide(_ sender: UIButton) {
+        touchUp(sender)
+    }
     
+    func toLogin()  {
+        
+    }
+    
+    func toRegister() {
 
+    }
     /*
     // MARK: - Navigation
 
